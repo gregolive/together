2b6867f3d37ce5f337b8f292f7be46a779b2cefa
Gregory Olive <gregoryolive@outlook.com>
2022-01-07 01:24:44 +0900

    Update post views, remove title from post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
+  # - RELATIONS
   belongs_to :user
+
+  # - VALIDATIONS
+  validates :body, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_06_065548) do
+ActiveRecord::Schema.define(version: 2022_01_06_104612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
     t.text "body"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
