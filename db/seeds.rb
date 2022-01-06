14004014e6a4300f1826c575e6923401ac7ec696
Gregory Olive <gregoryolive@outlook.com>
2022-01-06 15:36:51 +0900

    Update password views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'humblebragger@humblebrag.com', password: 'humblebaby')
+User.create(email: 'fitnessgrampacer@test.com', password: 'pacertest')
+User.create(email: 'greghockey54@hotmail.com', password: 'password')
