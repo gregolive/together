4d56e0996dd40efbb06daf56badac23202c07a6c
Gregory Olive <gregoryolive@outlook.com>
2022-01-10 15:09:53 +0900

    Create friendship model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_09_112204) do
+ActiveRecord::Schema.define(version: 2022_01_10_054702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2022_01_09_112204) do
     t.bigint "user_id", null: false
     t.integer "parent_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "likes", force: :cascade do |t|
