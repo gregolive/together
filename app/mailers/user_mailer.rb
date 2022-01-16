8352c03c0615bd3ab8ebc959994d197faec2a89e
Gregory Olive <gregoryolive@outlook.com>
2022-01-16 14:00:06 +0900

    Add mailer views

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: 'welcome@together.com'
+
+  def welcome_email
+    @user = params[:user]
+    @url  = 'http://localhost:3000/users/sign_in'
+    mail(to: @user.email, subject: 'Welcome to Together')
+  end
 end
