6fe0609c4f956e36fc41e026881546a56c8608fe
Gregory Olive <gregoryolive@outlook.com>
2022-01-20 13:32:04 +0900

    Update mailer for SendGrid

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: 'welcome@together.com'
+  default from: 'gregoryolive@outlook.com'
 
   def welcome_email
     @user = params[:user]
-    @url  = 'http://localhost:3000/users/sign_in'
+    @url  = 'https://together-social.herokuapp.com/users/sign_in'
     mail(to: @user.email, subject: 'Welcome to Together')
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,14 @@
+# SendGrid mailer
+ActionMailer::Base.smtp_settings = {
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: 'https://together-social.herokuapp.com/',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
+
 # Load the Rails application.
 require_relative "application"
 
